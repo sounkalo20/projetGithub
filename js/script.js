01c8291a67462faff2bfae67a3f72f3e7d80e992
sounkalo20 <sidibesounk2003@gmail.com>
2023-01-05 13:29:56 +0000

    finissions

--- js/script.js
+++ js/script.js
@@ -6,18 +6,26 @@ let inputBox = document.querySelector(".input-box"),
 searchIcon.addEventListener("click", () => inputBox.classList.add("open"));
 closeIcon.addEventListener("click", () => inputBox.classList.remove("open"));
 
-
-// on ajoute a lhistorique le mot saisie en fonction du resultat de la requete
-function listHistorique(e, stat){
-    let value;
-    if(stat){
-        value="historique-success";
+// on verifie la connexion
+function verifierConnexion(){
+    if(navigator.onLine){
+        $('.params .tools .fa-wifi').css("color", "#4070f4");   
+        $('.params .tools .fa-wifi').attr("title", "En ligne");   
     }
     else{
-        value="historique-failed";
+        $('.params .tools .fa-wifi').css("color", "red");   
+        $('.params .tools .fa-wifi').attr("title", "Hors ligne");   
     }
-    $('.historique ul').append("<li class='"+value+"'>"+e+"</li>");
 }
+
+// Pour fermer la zone de proposition
+$('#proposition .propositionCloser').click(function (e) { 
+    $(".propositionBackground").fadeOut(300);
+});
+
+setInterval(verifierConnexion, 30000);
+verifierConnexion();
+
 
 // pour afficher et masquer les parametres
 $('.params .fa-gear').click(function (e) { 
@@ -29,20 +37,6 @@ $('.params .fa-user').click(function (e) {
     $('.propositionBackground').fadeToggle();
 });
 
-// on verifie la connexion
-setInterval(verifierConnexion, 30000);
-verifierConnexion();
-
-function verifierConnexion(){
-    if(navigator.onLine){
-        $('.params .tools .fa-wifi').css("color", "#4070f4");   
-        $('.params .tools .fa-wifi').attr("title", "En ligne");   
-    }
-    else{
-        $('.params .tools .fa-wifi').css("color", "red");   
-        $('.params .tools .fa-wifi').attr("title", "Hors ligne");   
-    }
-}
 
 // affichage de lhistorique
 $('.historiqueToggler').click(function (e) { 
@@ -56,20 +50,22 @@ $('.projetToggler').click(function (e) {
     $('.project').toggle("slide");
 });
 
+//Pour Afficher le profil de l'utilisateur demander en fonction de son nom d'utilisateur
 function afficherProfil(nom){
     $.ajax({
         type: "GET",
         url: "https://api.github.com/users/"+nom,
         dataType: "JSON",
         success: function (e) {
-            
-             $('.profil .image img').attr("src",e.avatar_url);
-             $('.profil .name').text(e.login);
-             $('.profil .bio span').text(e.bio);
-             $('.profil .follow .followers').text(e.followers);
-             $('.profil .follow .following').text(e.following);
-             $('.profil .follow .public_repos').text(e.public_repos);
+            //on recupere ses information a part ses projets
+            $('.profil .image img').attr("src",e.avatar_url);
+            $('.profil .name').text(e.login);
+            $('.profil .bio span').text(e.bio);
+            $('.profil .follow .followers').text(e.followers);
+            $('.profil .follow .following').text(e.following);
+            $('.profil .follow .public_repos').text(e.public_repos);
 
+            // on recupere ses projets
             $.ajax({
                 type: "GET",
                 url: "https://api.github.com/users/"+nom+"/repos",
@@ -85,26 +81,57 @@ function afficherProfil(nom){
                 }
             });
 
+            //on archive dans l'historique avec un succes
             listHistorique(nom, true);
 
         },
         error: function(){
+            //on archive dans l'historique avec une erreur
             listHistorique(nom, false);
-
         }
     });
-}
 
+    // on ajoute a lhistorique le mot saisie en fonction du resultat de la requete
+    function listHistorique(e, stat){
+        let value;
+        if(stat){
+            value="historique-success";
+        }
+        else{
+            value="historique-failed";
+        }
+        $('.historique ul').append("<li class='"+value+"'>"+e+"</li>");
+    }
+}
+//on affiche un premiers utilisateurs par defauts
 afficherProfil("Abbv75");
 
+
+let inputVal=$('.input-text').val();
+$('.input-text').change(function (e) { 
+    inputVal=$('.input-text').val();
+});
+
+$('.search-btn').click(function (e) { 
+    $('.input-text').trigger("focus");
+    if(inputVal!=''){
+        afficherProfil(inputVal);
+    }
+    return false;
+});
+
+
+//une fonction qui permet de proposer des utilisateurs a partir d'un tableau donner
 function afficherUserProposition(){
-    tab=["abbv75","sounkalo20"];
+    tab=["abbv75","sounkalo20","as"];
+
     for(let i=0;i<tab.length;i++){
         $.ajax({
             type: "GET",
             url: "https://api.github.com/users/"+tab[i],
             dataType: "JSON",
             success: function (response) {
+                //on cree un composant qui contiendra les infos de l'utilisateur en fonction de l'index du tableau 
                 let component='<div class="propositionCard '+ tab[i]+'">'+
                     '<div style="display: flex; justify-content: center;">'+
                         '<div class="userImage">'+
@@ -121,6 +148,7 @@ function afficherUserProposition(){
 
                 $('.propositionMain').append(component);
 
+                //on recupere les projets de l'utilisateur courant
                 $.ajax({
                     type: "GET",
                     url: "https://api.github.com/users/"+tab[i]+"/repos",
@@ -139,31 +167,19 @@ function afficherUserProposition(){
                         }
                     }
                 });
+
+                //on ferme la div et on affiche les infos de l'utilisateur courant 
                 $('.'+tab[i]+" .voirPlus").click(function (e) { 
                     afficherProfil(tab[i]);
                     $(".propositionBackground").fadeOut(300);
                 });
             }
-        });
-        
-    }
-
-    
-    
+        });  
+    }    
 }
 afficherUserProposition()
 
-let inputVal=$('.input-text').val();
-$('.input-text').change(function (e) { 
-    inputVal=$('.input-text').val();
-});
 
-$('.search-btn').click(function (e) { 
-    if(inputVal!=''){
-        afficherProfil(inputVal);
-    }
-    return false;
-});
 
 let backgroundcolor1=$("#backgroundColor1").val();
 let backgroundcolor2=$("#backgroundColor2").val();
@@ -178,37 +194,28 @@ $("#backgroundColor2").on("input", function () {
 $(".background input").on("input", function () {
     changeBackgroundColor();
 });
+
 let rotate1=0;
 let rotate2=0;
 let rotate3=0;
-$(".backgroundReset").click(function (e) { 
-    backgroundcolor1="rgba(171, 4, 171, 0.699)"
-    backgroundcolor2="rgba(5, 5, 172, 0.722)"
-    changeBackgroundColor();
-    rotate1+=180;
-    $(this).css("transform", "rotate("+parseInt(rotate1)+"deg)");
-});
 
+//gestion theme fond d'ecran
 function changeBackgroundColor(){
     $(".backgroundColor1").css("background", backgroundcolor1);
     $(".backgroundColor2").css("background", backgroundcolor2);
     $("body").css("background", "linear-gradient(45deg,"+backgroundcolor1+" 40%, "+backgroundcolor2);
 }
 
+$(".backgroundReset").click(function (e) { 
+    backgroundcolor1="rgba(171, 4, 171, 0.699)"
+    backgroundcolor2="rgba(5, 5, 172, 0.722)"
+    changeBackgroundColor();
+    rotate1+=180;
+    $(this).css("transform", "rotate("+rotate1+"deg)");
+});
+
+
 //change text color
-let textColor1=$("#textColor1").val();
-
-$("#textColor1").on("input", function () {
-     textColor1=$("#textColor1").val();
-     changeTextColor(textColor1)
-});
-let textColor2=$("#textColor2").val();
-
-$("#textColor2").on("input", function () {
-    textColor2=$("#textColor2").val();
-     changeTextColor2(textColor2)
-});
-
 function changeTextColor(value){
     $('.profil .image img').css("border-color",value)
     $('#title .image').css("border-color",value)
@@ -221,7 +228,19 @@ function changeTextColor2(value){
     $('.allText').css("color",value)
     $(".textColor2").css("background", value);
 }
-    
+
+let textColor1=$("#textColor1").val();
+$("#textColor1").on("input", function () {
+    textColor1=$("#textColor1").val();
+    changeTextColor(textColor1)
+});
+
+let textColor2=$("#textColor2").val();
+$("#textColor2").on("input", function () {
+    textColor2=$("#textColor2").val();
+    changeTextColor2(textColor2)
+});
+
 $(".textReset").click(function (e) { 
     rotate2+=180;
     $(this).css("transform", "rotate("+parseInt(rotate2)+"deg)");
@@ -230,19 +249,6 @@ $(".textReset").click(function (e) {
 });
 
 //components color
-
-let componantColor1=$("#composantColor1").val();
-let componantColor2=$("#composantColor2").val();
-
-$("#composantColor1").on("input", function () {
-    componantColor1=$("#composantColor1").val()
-    changeComposentColor1(componantColor1);
-});
-$("#composantColor2").on("input", function () {
-    componantColor2=$("#composantColor2").val()
-    changeComposentColor2(componantColor2);
-});
-
 function changeComposentColor1(value){
     $(".componantBackgroundColor").css("background", value);
     $(".componantIconColor i").css("color", value);
@@ -252,6 +258,19 @@ function changeComposentColor2(value){
     $(".composantColor2").css("background", value);
     $(".allBackgroundColor").css("background", value);
 }
+
+let componantColor1=$("#composantColor1").val();
+let componantColor2=$("#composantColor2").val();
+
+$("#composantColor1").on("input", function () {
+    componantColor1=$("#composantColor1").val()
+    changeComposentColor1(componantColor1);
+});
+
+$("#composantColor2").on("input", function () {
+    componantColor2=$("#composantColor2").val()
+    changeComposentColor2(componantColor2);
+});
 
 $(".composantReset").click(function (e) { 
     rotate3+=180;
